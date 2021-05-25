4360b1a95312682637dcd048a0eb269151200396
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-25 16:28:40 +0900

    feat: redux-saga 설정에서 빼먹은 부분 보충
    - root-saga 파일 생성 및 store에 development 모드일 경우 saga middleware apply
    - store의 sataTask에 적용할 root-saga를 별도의 파일로 작성

--- client/redux/store.js
+++ client/redux/store.js
@@ -3,6 +3,7 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducer from './root-reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import rootSaga from './root-saga';
 
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
@@ -10,8 +11,9 @@ const configureStore = () => {
     const enhancer = process.env.NODE_ENV === 'production' ?
         // 개발시에 필요한 middleware 추가 (redux-saga, thunk)
         compose(applyMiddleware(...middleware)) :
-        composeWithDevTools(applyMiddleware());
+        composeWithDevTools(applyMiddleware(...middleware));
     const store = createStore(rootReducer, enhancer);
+    store.sataTask = sagaMiddleware.run(rootSaga);
     return store;
 };
 
